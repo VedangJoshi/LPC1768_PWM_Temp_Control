43594166afc594d6bff94a06ccc22bb85cc6e26e
Vedang Joshi <VedangJoshi@users.noreply.github.com>
2020-06-13 01:07:37 -0700

    Update Fan_Curve_Table.h

--- Fan_Curve_Table.h
+++ Fan_Curve_Table.h
@@ -12,12 +12,12 @@
 #include "TextLCD.h"
 #include "IAP.h"
 
-#define CONFIG 0x01
-#define BIT_RES_12 0x60
-#define MODE_4HZ 0xA0
-#define TABLE_SIZE 6
-#define	MEM_SIZE 4096
-#define TARGET_SECTOR 29
+#define CONFIG		0x01
+#define BIT_RES_12	0x60
+#define MODE_4HZ 	0xA0
+#define TABLE_SIZE 	6
+#define	MEM_SIZE 	4096
+#define TARGET_SECTOR 	29
 
 const float sensistivity = 0.0625;
 const int addr = 0x90;
@@ -44,7 +44,11 @@ struct fan_curve_table_element {
 };
 
 struct fan_curve_table_element fan_curve_table[TABLE_SIZE] =
-		{ { 10.0, 0.0 }, { 20.0, 0.4 }, { 25.0, 0.6 }, { 30.0, 0.7 }, { 35.0,
-				0.8 }, { 45.0, 1.0 } };
+		{ { 10.0, 0.0 }, 
+		  { 20.0, 0.4 }, 
+		  { 25.0, 0.6 }, 
+		  { 30.0, 0.7 }, 
+		  { 35.0, 0.8 }, 
+		  { 45.0, 1.0 } };
 
 #endif /* FAN_CURVE_TABLE_H_ */
